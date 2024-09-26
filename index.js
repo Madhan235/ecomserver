6fb8612f3f5b8ea6fd22e8bc1591737b02064f3a
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-09-26 12:58:32 +0530

    cors added

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import dataRouter from "./routes/data.routes.js";
-// import cors from "cors";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
-
-// app.use(
-//   cors({
-//     origin: ["https://ecom96.netlify.app/", "http://localhost:3000"],
-//     methods: ["GET", "POST"],
-//     credentials: true,
-//   })
-// );
 
 app.use("/api", dataRouter);
 
